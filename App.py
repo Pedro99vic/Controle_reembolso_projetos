356357531237e073697f099186265e27dba8d877
Pedro <Pedro99vic>
2026-03-05 15:34:50 -0300

    substituição de textos desnecessários na coluna subcategoria

--- App.py
+++ App.py
@@ -9,7 +9,7 @@ import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8') # locale - Brasil
 
 # LEITURA DA BASE DE DADOS
-df = pd.read_excel(r"C:\Users\Projexa\OneDrive - projexa.com.br\Área de Trabalho\Estudos_python\1.Dashboard_gastos_projetos\data\03-03-2026 20_19_16.xlsx")
+df = pd.read_excel(r"C:\Users\Projexa\OneDrive - projexa.com.br\Área de Trabalho\Estudos_python\1.Dashboard_reembolso_projetos\data\03-03-2026 20_19_16.xlsx")
 
 # TRATAMENTO DE DADOS
 # 1 - EXCLUI COLUNAS DESNECESSÁRIAS
@@ -39,6 +39,22 @@ df5["MES_ANO"] = df5["MES_ANO"].dt.strftime("%m/%Y")
 df6 = df5
 df6["VALOR"] = pd.to_numeric(df6["VALOR"], errors="coerce")
 
+# 7 - SUBISTITUI TEXTOS DESNECESSÁRIOS DA COLUNA "SUBCATEGORIA"
+df7 = df6
+substituicoes = {
+    "Hotel (OBRIGATÓRIO JUSTIFICATIVA)": "Hotel",
+    "Lavanderia - obrigatório inserir o período": "Lavanderia",
+    "Material ou Suprimento (CUSTO EMPRESA - OBRIGATÓRIO JUSTIFICATIVA)" : "Material ou Suprimento",
+    "Passagem Aérea (OBRIGATÓRIO JUSTIFICATIVA)":"Passagem Aérea",
+    "Material de Limpeza - somente para casa alugada - obrigatório NOME DE QUEM AUTORIZOU":"Material de Limpeza",
+    "Passagens de Ônibus (OBRIGATÓRIO JUSTIFICATIVA)":"Passagens de Ônibus",
+    "Locação de casa - OBRIGATÓRIO NOME DE QUEM AUTORIZOU":"Locação de casa"
+}
+df7["SUBCATEGORIA"] = df7["SUBCATEGORIA"].replace(substituicoes)
+
+
+# ... - ULTIMA ATUALIZAÇÃO NO NOME DF
+df = df7
 
 # CONFIGURAÇÃO DA PÁGINA
 st.set_page_config(layout="wide")
@@ -52,7 +68,7 @@ col1, col2 = st.columns(2) # layout dos filtros
 with col1:
     projetos = st.pills(
         "Projetos:",
-        df6["PROJETO"].dropna().unique(),
+        df["PROJETO"].dropna().unique(),
         selection_mode = "multi",
         label_visibility = "collapsed"
     )
@@ -60,7 +76,7 @@ with col1:
 with col2:
     meses = st.pills(
         "Meses:", 
-        df6["MES_ANO"].dropna().unique(),
+        df["MES_ANO"].dropna().unique(),
         selection_mode = "multi",
         label_visibility = "collapsed"
     )
@@ -71,9 +87,9 @@ if not projetos or not meses:
     st.stop()
 
 # DATA FRAME COM OS VALORES FILTRADOS
-df_filtrado = df6[
-    (df6["PROJETO"].isin(projetos)) &
-    (df6["MES_ANO"].isin(meses))
+df_filtrado = df[
+    (df["PROJETO"].isin(projetos)) &
+    (df["MES_ANO"].isin(meses))
 ]
 
 # AGRUPAMENTO GRÁFICO 1
@@ -114,3 +130,5 @@ with col4:
         label="Total Gasto",
         value=f"R$ {df_soma:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
     )
+
+st.write(df["SUBCATEGORIA"].unique())
