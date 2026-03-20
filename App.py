8a1aedf60d8257d7ee08ed354a6242d44815a661
Pedro <pedrovictor.figueira@gmail.com>
2026-03-20 08:23:08 -0300

    adaptação para funcionar com modulos novos

--- App.py
+++ App.py
@@ -9,47 +9,20 @@ import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8') # locale - Brasil
 
 # LEITURA DA BASE DE DADOS
-df = pd.read_excel(r"C:\Users\Projexa\OneDrive - projexa.com.br\Área de Trabalho\Estudos_python\1.Dashboard_reembolso_projetos\data\base_de_dados_geral.xlsx")
+from data_loader import load_data
+df_raw = load_data(r"C:\Users\Projexa\OneDrive - projexa.com.br\Área de Trabalho\Estudos_python\1.Dashboard_reembolso_projetos\data\base_de_dados_geral.xlsx")
 
-###################################################################################################
 
 # TRATAMENTO DE DADOS
+from data_processing import data_precessing
+df_geral, df = data_precessing(df_raw)
 
-df = df.rename(columns={'# RELATÓRIO':'RELATÓRIO',
-                        'DATA DA DESPESA':'MES_ANO'}) # Correção dos nomes de algumas colunas
-
-df = df[['RELATÓRIO','USUÁRIO','MES_ANO',
-         'VALOR','CATEGORIA','SUBCATEGORIA',
-         'PROJETO','DESCRIÇÃO','APROVADORES']] # Seleção de colunas que serão utilizadas
-
-df["MES_ANO"] = pd.to_datetime(df["MES_ANO"], format="%d/%m/%Y") # Cria coluna mes_ano
-df["MES_ANO"] = df["MES_ANO"].dt.strftime("%m/%Y")
-
-# 3 - SUBSTITUI NaN POR "Outros"
-df = df.fillna("Outros") # Subistitui NaN por "Outros"
-df["VALOR"] = pd.to_numeric(df["VALOR"], errors="coerce") # Garante valores numéricos
-df["PROJETO"] = (df["PROJETO"].str.strip()) # Tira espaços da celulas da coluna PROJETO
-
-# 6 - SUBISTITUI TEXTOS DESNECESSÁRIOS DA COLUNA "SUBCATEGORIA"
-substituicoes = {
-    "Hotel (OBRIGATÓRIO JUSTIFICATIVA)": "Hotel",
-    "Lavanderia - obrigatório inserir o período": "Lavanderia",
-    "Material ou Suprimento (CUSTO EMPRESA - OBRIGATÓRIO JUSTIFICATIVA)" : "Material ou Suprimento",
-    "Passagem Aérea (OBRIGATÓRIO JUSTIFICATIVA)":"Passagem Aérea",
-    "Material de Limpeza - somente para casa alugada - obrigatório NOME DE QUEM AUTORIZOU":"Material de Limpeza",
-    "Passagens de Ônibus (OBRIGATÓRIO JUSTIFICATIVA)":"Passagens de Ônibus",
-    "Locação de casa - OBRIGATÓRIO NOME DE QUEM AUTORIZOU":"Locação de casa",
-    "Outro (Obrigatório Justificativa)":"Outros"
-}
-df["SUBCATEGORIA"] = df["SUBCATEGORIA"].replace(substituicoes)
-
-###################################################################################################
 
 # CONFIGURAÇÃO DA PÁGINA
 st.set_page_config(layout="wide")
 st.title("📊 Controle de Gastos e Reembolsos - PROJEXA")
 
-# FILTROS (PILLS STYLE)
+# FILTROS
 st.subheader("Filtros")
 
 col1, col2 = st.columns(2) # layout dos filtros
@@ -91,7 +64,7 @@ df_graf1 = (
 # AGRUPAMENTO METRIC
 df_soma = (df_filtrado["VALOR"].sum())
 
-col3, col4 = st.columns([4,1]) # layout dados
+col3, col4 = st.columns([3,1]) # layout dados
 
 # GRÁFICO DE BARRAS (COM PLOTLY)
 with col3:
@@ -116,5 +89,11 @@ with col3:
 with col4:
     st.metric(
         label="Total Gasto",
-        value=f"R$ {df_soma:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+        value=f"R$ {df_soma:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."),
+        border=True
     )
+
+
+# TABELA AO FINAL DA PAGINA (RAW DATA)
+st.subheader("Raw data - Filtrado")
+st.dataframe(df_filtrado)
